2a919a4f12a69b593b23b82dd0c303810b22dae7
Steven Ingham <stevenjingham@gmail.com>
2025-09-26 12:55:25 +0100

    Using caching

--- src/main/java/com/example/math_api/MathController.java
+++ src/main/java/com/example/math_api/MathController.java
@@ -1,18 +1,16 @@
 package com.example.math_api;
 
-import org.springframework.http.HttpEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MathController {
-    private final MathService mathService;
+public class MathController{
 
-    public MathController(){
-        this.mathService = new MathService();
-    }
+    @Autowired
+    MathService mathService;
 
     @GetMapping(path = "/primeNumber/{number}", produces = "application/json")
     public ResponseEntity<MathDTO> primeNumber(@PathVariable int number){
